45a1ec6471668ae5a2a80b31849bf70308838f23
Gaye Y <96841321+gaiaysl@users.noreply.github.com>
2022-05-13 17:14:28 +0300

    first commit

--- components/Product/Product.js
+++ components/Product/Product.js
@@ -1,34 +1,8 @@
-/*
-  This example requires Tailwind CSS v2.0+
 
-  This example requires some changes to your config:
+import styles from "./SingleProduct.module.scss"
 
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/aspect-ratio'),
-    ],
-  }
-  ```
-*/
-import styles from "../Product/Product.module.scss"
-const products = [
-    {
-        id: 1,
-        name: 'Basic Tee',
-        href: '#',
-        imageSrc: 'https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg',
-        imageAlt: "Front of men's Basic Tee in black.",
-        price: '$35',
-        color: 'Black',
-    },
-    // More products...
-]
 
-export default function Product() {
+export default function SingleProduct() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
